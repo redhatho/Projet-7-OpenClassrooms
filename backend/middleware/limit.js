67c879dd960c51fe79d0042710d3ce00b439cd70
redhatho <94616921+redhatho@users.noreply.github.com>
2022-07-16 17:48:41 +0200

    Delete backend directory

--- backend/middleware/limit.js
+++
@@ -1,9 +0,0 @@
-const rateLimit = require("express-rate-limit")
-
-const limiter = rateLimit({
-    windowMs: 5 * 60 * 1000,
-    max: 3,
-    message: "Votre accès a été bloqué pendant 5 minutes pour cause de connexions intempestives"
-})
-
-module.exports = { limiter }
